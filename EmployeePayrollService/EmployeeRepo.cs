15859bfd7b99526221cfe810032ada0362282f1b
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-23 20:44:57 +0530

    [Apoorva] Add . Ability to create a payroll service database and have java program connect to database

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Payroll_Service;Integrated Security=True";
+        readonly SqlConnection sqlconnection = new SqlConnection(connectionString);
+    }
+}
