1b3368fb1abb270d6c010458911cf540afc953d6
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-23 20:10:54 +0530

    [Apoorva] Add . Welcome to Employee Payroll Service

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service!");
+        }
+    }
+}
