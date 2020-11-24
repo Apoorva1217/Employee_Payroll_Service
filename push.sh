65bb6eeb422e1b1b9abbad61dfa07ce2f3e3fcb9
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-24 17:18:19 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Ability to retrieve all employees who have joined in a particular data range from the payroll service database"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
