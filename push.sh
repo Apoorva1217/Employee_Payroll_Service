e087719ac5ddd0441bfa8ac1d904b91aa33d3b8c
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-24 17:58:48 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Ability to find sum, average, min, max and number of male and female employees"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
