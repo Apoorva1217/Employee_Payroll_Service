957e0fadcb084c60e6ae6c77d258000f18812f8f
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-25 15:21:27 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Refactor . Refactored query with new table structure"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
