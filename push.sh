ce8ceb88c0e5515da1665a1ac4256c68f119629b
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-29 13:52:30 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Ability to add multiple employee with thread with synchronization"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
