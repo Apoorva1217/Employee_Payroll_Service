a8e31d70396dc469fda7a9a39421537df84467a2
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-12-01 17:47:33 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Ability to Delete Employee from EmployeePayroll JSON Server"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
